fa12b94406435502d08eb049f88e819ec3a17425
Aayush Acharya <aayushacharya007@gmail.com>
2025-09-14 15:29:24 -0500

    Initial commit of Project 03

---
+++ Skeleton/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Skeleton;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ Skeleton/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Skeleton;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
